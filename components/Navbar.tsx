fa66e5eace05b768a1486815923e91af46bea94c
Hayotunday <idowudanielayotunde@gmail.com>
2023-08-15 20:13:46 +0100

    commit before I install ZorinOS

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,12 @@ import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-	const session = {};
+const Navbar = async () => {
+	const session = await getCurrentUser();
 
 	return (
 		<nav className="flexBetween navbar">
@@ -19,17 +22,18 @@ const Navbar = () => {
 						</Link>
 					))}
 				</ul>
+			</div>
 
-				<div className="flexCenter gap-4">
-					{session ? (
-						<>
-							UserPhoto
-							<Link href={"/create-project"}>Share Work</Link>
-						</>
-					) : (
-						<AuthProviders />
-					)}
-				</div>
+			<div className="flexCenter gap-4">
+				{session?.user ? (
+					<>
+						<ProfileMenu session={session} />
+
+						<Link href={"/create-project"}>Share Work</Link>
+					</>
+				) : (
+					<AuthProviders />
+				)}
 			</div>
 		</nav>
 	);
